c4d265cc3031a51ecb38e82f9ed925d73f19b1aa
xllei <xllei@Ctrip.com>
2016-11-04 15:02:16 +0800

    Add new extensions.

---
+++ CodeSnippet/Extensions/ByteExtension.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lino.CodeSnippet.Extensions
+{
+    /// <summary>
+    /// 原文参考：http://www.cnblogs.com/ldp615/archive/2009/08/14/1552133.html
+    /// </summary>
+    public static class ByteExtension
+    {
+        /// <summary>
+        /// 转换为十六进制
+        /// </summary>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static string ToHex(this byte b)
+        {
+            return b.ToString("X2");
+        }
+
+        /// <summary>
+        /// 转换为十六进制
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public static string ToHex(this IEnumerable<byte> bytes)
+        {
+            var sb = new StringBuilder();
+            foreach (byte b in bytes)
+                sb.Append(b.ToString("X2"));
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 转换为Base64字符串
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public static string ToBase64String(byte[] bytes)
+        {
+            return Convert.ToBase64String(bytes);
+        }
+
+        /// <summary>
+        /// 转换为Int32
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="startIndex"></param>
+        /// <returns></returns>
+        public static int ToInt(this byte[] value, int startIndex)
+        {
+            return BitConverter.ToInt32(value, startIndex);
+        }
+
+        /// <summary>
+        /// 转换为Int64
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="startIndex"></param>
+        /// <returns></returns>
+        public static long ToInt64(this byte[] value, int startIndex)
+        {
+            return BitConverter.ToInt64(value, startIndex);
+        }
+
+        /// <summary>
+        /// 转换为指定编码的字符串
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static string Decode(this byte[] data, Encoding encoding)
+        {
+            return encoding.GetString(data);
+        }
+
+        /// <summary>
+        /// 使用指定算法Hash
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="hashName"></param>
+        /// <returns></returns>
+        public static byte[] Hash(this byte[] data, string hashName)
+        {
+            HashAlgorithm algorithm;
+            if (string.IsNullOrEmpty(hashName)) algorithm = HashAlgorithm.Create();
+            else algorithm = HashAlgorithm.Create(hashName);
+            return algorithm.ComputeHash(data);
+        }
+
+        /// <summary>
+        /// 使用默认算法Hash
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static byte[] Hash(this byte[] data)
+        {
+            return Hash(data, null);
+        }
+
+
+        /// <summary>
+        /// 位运算，index从0开始，获取取第index是否为1
+        /// </summary>
+        /// <param name="b"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public static bool GetBit(this byte b, int index)
+        {
+            return (b & (1 << index)) > 0;
+        }
+
+        /// <summary>
+        /// 将第index位设为1
+        /// </summary>
+        /// <param name="b"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public static byte SetBit(this byte b, int index)
+        {
+            b |= (byte)(1 << index);
+            return b;
+        }
+
+        /// <summary>
+        /// 将第index位设为0
+        /// </summary>
+        /// <param name="b"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public static byte ClearBit(this byte b, int index)
+        {
+            b &= (byte)((1 << 8) - 1 - (1 << index));
+            return b;
+        }
+
+        /// <summary>
+        /// 将第index位取反
+        /// </summary>
+        /// <param name="b"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public static byte ReverseBit(this byte b, int index)
+        {
+            b ^= (byte)(1 << index);
+            return b;
+        }
+
+        /// <summary>
+        /// 保存为文件
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="path"></param>
+
+        public static void Save(this byte[] data, string path)
+        {
+            File.WriteAllBytes(path, data);
+        }
+
+        /// <summary>
+        /// 转换为内存流
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static MemoryStream ToMemoryStream(this byte[] data)
+        {
+            return new MemoryStream(data);
+        }
+
+    }
+}
---
+++ CodeSnippet/Extensions/RandomExtension.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lino.CodeSnippet.Extensions
+{
+    public static class RandomExtension
+    {
+        /// <summary>
+        /// 随机返回 true 或 false
+        /// </summary>
+        /// <param name="random"></param>
+        /// <returns></returns>
+        public static bool NextBool(this Random random)
+        {
+            return random.NextDouble() >= 0.5;
+        }
+
+        /// <summary>
+        /// 返回随机枚举项
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="random"></param>
+        /// <returns></returns>
+        public static T NextEnum<T>(this Random random)
+            where T : struct
+        {
+            Type type = typeof(T);
+            if (type.IsEnum == false) throw new InvalidOperationException();
+
+            var array = Enum.GetValues(type);
+            var index = random.Next(array.GetLowerBound(0), array.GetUpperBound(0) + 1);
+            return (T)array.GetValue(index);
+        }
+
+        /// <summary>
+        ///  返回指定长度的byte数组
+        /// </summary>
+        /// <param name="random"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static byte[] NextBytes(this Random random, int length)
+        {
+            var data = new byte[length];
+            random.NextBytes(data);
+            return data;
+        }
+    }
+}
